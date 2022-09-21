76661bafc43cdff48006e7dd9c2d6deb3531db2b
Julian Canepa <julian.l.canepa@gmail.com>
2022-09-21 11:33:04 -0700

    Begin exercise 0, creating main and MathBot ruby classes. Game play done, ready to code some math calculations.

---
+++ main.rb
@@ -0,0 +1,42 @@
+require_relative "mathbot"
+
+class GamePlayController
+    @@options = ['add', 'sub', 'mul', 'div', 'quit']
+
+    def run
+        continue = true
+
+        while continue
+            print 'SuperMathyBot> '
+
+            # return process_command()
+            command = gets.split(" ", 3)
+
+            # first command is not an option
+            if !@@options.include?(command[0])
+                print_help()
+                next
+            end
+
+            # allow user to terminate the app
+            if command[0].eql?('quit')
+                continue = false
+            else
+                # hand off the calculation to SuperMathyBot
+                puts 'I guess I\'ll do some math'
+            end
+        end
+    end
+
+    def print_help
+        puts 'usage: add|sub|mul|div v0 v1'
+        puts '       quit'
+    end
+end
+
+GamePlayController.new.run()
+# Show "SuperMathyBot" command prompt
+# accept command and two optional numbers
+# validate command (quit ends the program)
+# process arithmatic commands using mathbot
+# answer returned by mathbot is a float
